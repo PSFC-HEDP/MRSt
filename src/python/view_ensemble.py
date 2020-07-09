b58cdbff6b3497c2a393521f6748f9c7efad02e2
Justin Kunimune <justinkunimune@gmail.com>
2020-07-09 16:02:49 -0400

    The Big Graph
    I formatted my really big plot for use in a papre. It might actually fit pretty decently on a page.

--- src/python/view_ensemble.py
+++ src/python/view_ensemble.py
@@ -1,25 +1,31 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.family': 'serif', 'font.size': 10})
 
 X_LABEL = "Yield factor"
 
 Y_LABELS = [
-	("Bang time (ns)", 16.364), ("Max ρR (ns)", 16.310), ("Max dρR/dt (ns)", 16.243),
-	("Ti at BT (keV)", 10.552), ("ρR at BT (g/cm^2)", 1.069), ("vi at BT (μm/ns)", 10.595),
+	("Bang time (ns)", 16.364), ("Max ρR - BT (ps)", 16.310), ("Max dρR/dt - BT (ps)", 16.243),
+	("Ti at BT (keV)", 10.552),
+	# ("ρR at BT (g/cm^2)", 1.069),
+	("vi at BT (μm/ns)", 10.595),
 	("dTi/dt at BT (keV/ns)", 107.44), ("dρR/dt at BT (g/cm^2/ns)", -8.05),
 	("dvi/dt at BT (μm/ns^2)", -1130), ("Max ρR (g/cm^2)", 1.426),
 	("Total yield", 5.37e17), ("Burn mean (ns)", 16.357),
 	("Burn width (ns)", .0666), ("Burn skew", -1.11), ("Burn kurtosis", 6.32)
 ]
 
-COLUMNS = 3
+COLUMNS = 2
 
 simulations = pd.read_csv('../../working/ensemble.csv')
 simulations["Total yield"] = simulations["Total yield (10^15)"]*1e15
+for key in simulations:
+	if "(ns)" in key:
+		simulations[key[:-4]+"- BT (ps)"] = (simulations[key] - simulations["Bang time (ns)"])*1e3
 
-fig, axs = plt.subplots((len(Y_LABELS) + COLUMNS-1)//COLUMNS, COLUMNS)
-fig.subplots_adjust(hspace=0)
+fig, axs = plt.subplots((len(Y_LABELS) + COLUMNS-1)//COLUMNS, COLUMNS, figsize=(8, 10.5))
+fig.subplots_adjust(bottom=.05, top=.95, left=.10, right=.99, wspace=.30, hspace=.00)
 
 for i, (axis, true) in enumerate(Y_LABELS):
 	if 'keV' in axis:      yFactor = simulations["Temperature factor"]
@@ -52,4 +58,5 @@ for i, (axis, true) in enumerate(Y_LABELS):
 		ax.xaxis.set_visible(False)
 	ax.set_ylabel(axis)
 
+# plt.savefig('../../working/big-plot.eps')
 plt.show()
