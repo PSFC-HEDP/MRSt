c1dcd95555843fa4bf98db83c709e4a6bcba7c0f
Justin Kunimune <justinkunimune@gmail.com>
2020-07-21 12:02:57 -0400

    Plotting issues
    The plots were just a little bit wrong.

--- src/python/view_ensemble.py
+++ src/python/view_ensemble.py
@@ -6,7 +6,7 @@ plt.rcParams.update({'font.family': 'serif', 'font.size': 10})
 X_LABEL = "Yield factor"
 
 Y_LABELS = [
-	("Bang time (ns)", 16.364), ("Max ρR - BT (ps)", 16.304), ("Max dρR/dt - BT (ps)", 16.243),
+	("Bang time (ns)", 16.364), ("Max ρR - BT (ps)", -60), ("Max dρR/dt - BT (ps)", -121),
 	("Ti at BT (keV)", 10.40),
 	# ("ρR at BT (g/cm^2)", 1.133),
 	("vi at BT (μm/ns)", 13.86),
@@ -29,7 +29,7 @@ for key in simulations:
 			simulations[key[:-4]+"- BT (ps)"] = (simulations[key] - simulations["Bang time (ns)"])*1e3
 
 fig, axs = plt.subplots((len(Y_LABELS) + COLUMNS-1)//COLUMNS, COLUMNS, figsize=(8, 10.5))
-fig.subplots_adjust(bottom=.05, top=.95, left=.10, right=.99, wspace=.30, hspace=.00)
+fig.subplots_adjust(bottom=.05, top=.95, left=.10, right=.99, wspace=.30, hspace=.05)
 
 for i, (axis, true) in enumerate(Y_LABELS):
 	if 'keV' in axis:      yFactor = simulations["Temperature factor"]
@@ -42,9 +42,10 @@ for i, (axis, true) in enumerate(Y_LABELS):
 	ax = axs[i//COLUMNS,i%COLUMNS]
 	ax.scatter(simulations[X_LABEL], simulations[axis], s=10, zorder=1, label="Based on fit to synthetic data")
 	ax.set_ylim(auto=False)
-	ax.errorbar(simulations[X_LABEL], simulations[axis], yerr=simulations[axis+" error"], linestyle='none')
+	ax.errorbar(simulations[X_LABEL], simulations[axis], yerr=simulations[axis+" error"], elinewidth=1, linestyle='none')
 	if simulations[axis].min() > 0 and simulations[axis].max()/simulations[axis].min() >= 100:
 		ax.set_yscale('log')
+		ax.set_ylim(simulations[axis].min()/1.5, simulations[axis].max()*1.5)
 	ax.plot(simulations[X_LABEL][order], yFactor[order]*true, 'C1--', zorder=0, label="Based on original data")
 	if 'ield' in X_LABEL:
 		ax.set_xscale('log')
