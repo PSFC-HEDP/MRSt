48fe702922afdb03b15f603c294cd31215cdb594
Justin Kunimune <justinkunimune@gmail.com>
2020-08-28 10:28:10 -1000

    Close enough
    I added top-level physics requirements to the plots.

--- src/python/view_ensemble.py
+++ src/python/view_ensemble.py
@@ -7,12 +7,12 @@ plt.rcParams.update({'font.family': 'serif', 'font.size': 10})
 X_LABEL = "Yield factor"
 
 Y_LABELS = [
-	("Total yield", 2e14, 4.7838e17, 9e18, 1e-4, True), ("Total yield", 2e14, 4.7838e17, 9e18, 1e-4, True),
-	("Burn-average ρR (g/cm^2)", 0.3, .6900, 1.1, 3e-3, True), ("Burn-average Ti (keV)", 8.8, 10.0198, 11.2, 3e-3, True),
-	("Bang time (ns)", 16.349, 16.3645, 16.371, 3e-4, False),	("Burn width (ps)", 58, 66.732, 77, 1e-1, False),
-	("Burn skew", -2.1, -1.1469, 0.11, 1e-2, False), ("Burn kurtosis", 3.8, 6.5166, 9.2, 1e-1, False),
-	("dρR/dt at BT (mg/cm^2/(100ps))", -850, -282.4, 450, 3e1, False), ("dTi/dt at BT (keV/(100ps))", -0.2, 6.467, 12.2, 3e-1, False),
-	("Burn-average vi (km/s)", -20.2, 1.734, 20.2, 3e-1, False), ("dvi/dt at BT (km/s/(100ps))", -170, -72.19, 20, 3e0, False)
+	("Total yield", 2e14, 4.7838e17, 9e18, 5e-2, True), ("Total yield", 2e14, 4.7838e17, 9e18, 5e-2, True),
+	("Burn-average ρR (g/cm^2)", 0.3, .6900, 1.1, 7e-2, True), ("Burn-average Ti (keV)", 8.8, 10.0198, 11.2, 7e-2, True),
+	("Bang time (ns)", 16.349, 16.3645, 16.371, 1e-2, False),	("Burn width (ps)", 58, 66.732, 77, 7, False),
+	("Burn skew", -2.1, -1.1469, 0.11, 3e-1, False), ("Burn kurtosis", 3.8, 6.5166, 9.2, 3, False),
+	("dρR/dt at BT (mg/cm^2/(100ps))", -850, -282.4, 450, 60, False), ("dTi/dt at BT (keV/(100ps))", -0.2, 6.467, 12.2, 1.9, False),
+	("Burn-average vi (km/s)", -20.2, 1.734, 20.2, 20, False), ("dvi/dt at BT (km/s/(100ps))", -170, -72.19, 20, 8, False)
 ]
 
 COLUMNS = 2
@@ -62,6 +62,12 @@ for i, (axis, y_min, y_true, y_max, presis, percent) in enumerate(Y_LABELS):
 
 	ax = axs_p[i//COLUMNS,i%COLUMNS]
 	valid = np.logical_not(np.isnan(simulations[axis+" error"]))
+	if not percent:
+		ax.fill_between(simulations[X_LABEL][order],
+			y_factor[order]*y_true - presis, y_factor[order]*y_true + presis, color='C1', alpha=0.3)
+	else:
+		ax.fill_between(simulations[X_LABEL][order],
+			y_factor[order]*y_true*(1 - presis), y_factor[order]*y_true*(1 + presis), color='C1', alpha=0.3)
 	ax.plot(simulations[X_LABEL][order], y_factor[order]*y_true, 'C1--', zorder=0, label="Based on original data")
 	ax.scatter(simulations[X_LABEL][valid], simulations[axis][valid], s=2, zorder=1, label="Based on fit to synthetic data")
 	# ax.errorbar(simulations[X_LABEL][valid], simulations[axis][valid], yerr=simulations[axis+" error"][valid], elinewidth=1, linestyle='none')
@@ -93,14 +99,16 @@ for i, (axis, y_min, y_true, y_max, presis, percent) in enumerate(Y_LABELS):
 	y_factor = np.interp(bin_centers, simulations[X_LABEL], y_factor)
 	if not percent:
 		ax.plot(bin_centers, stds, 'C0-', label="Standard deviation from actuality")
-		ax.plot(bin_centers, errs, 'C1--', label="Reported error bar size")
+		ax.plot(bin_centers, y_factor*presis, 'C1--', label="Required accuracy")
+		ax.plot(bin_centers, errs, 'C2--', label="Reported error bar size")
 	else:
 		ax.plot(bin_centers, stds/(y_factor*y_true), 'C0-', label="Standard deviation from actuality")
-		ax.plot(bin_centers, errs/(y_factor*y_true), 'C1--', label="Reported error bar size")
+		ax.plot(bin_centers, np.full(bin_centers.shape, presis), 'C1--', label="Required accuracy")
+		ax.plot(bin_centers, errs/(y_factor*y_true), 'C2--', label="Reported error bar size")
 	ax.set_yscale('log')
 	if 'ield' in X_LABEL:
 		ax.set_xscale('log')
-	ax.set_ylim(presis*0.8, presis*1.25e2)
+	ax.set_ylim(presis*1.6e-2, presis*2.5)
 	if i == 0:
 		ax.legend()
 	if i//COLUMNS == axs_p.shape[0]-1:
